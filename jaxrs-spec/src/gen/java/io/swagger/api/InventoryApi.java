9906098d8877ac3b9355f7b2e37e3fd7c16bf8f6
arthurdm <arthurdm@ca.ibm.com>
2017-06-05 18:14:49 +0000

    Generated code by SwaggerHub

--- jaxrs-spec/src/gen/java/io/swagger/api/InventoryApi.java
+++ jaxrs-spec/src/gen/java/io/swagger/api/InventoryApi.java
@@ -15,7 +15,7 @@ import java.util.List;
 @Api(description = "the inventory API")
 
 
-@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaJAXRSSpecServerCodegen", date = "2017-05-24T17:36:47.003Z")
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaJAXRSSpecServerCodegen", date = "2017-06-05T18:14:48.628Z")
 
 public class InventoryApi  {
 
